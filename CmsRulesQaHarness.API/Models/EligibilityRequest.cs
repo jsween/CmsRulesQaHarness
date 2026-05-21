41824638ca5c2867e750e4dfebc2e95215085896
Jon Sweeney <jonsweeney26@gmail.com>
2026-05-21 10:14:11 -0700

    feat: add eligibilityRequest model

---
+++ CmsRulesQaHarness.API/Models/EligibilityRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace CmsRulesQaHarness.API.Models
+{
+    /// <summary>
+    /// Represents an eligibility determination request containing demographic and financial
+    /// information used to evaluate CMS program eligibility.
+    /// </summary>
+    public class EligibilityRequest
+    {
+        public int Age { get; set; }
+        public int MonthlyIncome { get; set; }
+        public bool IsDisabled { get; set; }
+        public bool IsPregnant { get; set; }
+        public bool IsBlind { get; set; }
+        public int HouseholdSize { get; set; }
+    }
+}
