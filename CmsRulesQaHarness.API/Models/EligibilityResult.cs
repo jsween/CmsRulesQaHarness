fe5ba4602cd3e7e84549dc2a6ceda1dad0aea556
Jon Sweeney <jonsweeney26@gmail.com>
2026-05-21 10:19:40 -0700

    feat: add eligibilityResult

---
+++ CmsRulesQaHarness.API/Models/EligibilityResult.cs
@@ -0,0 +1,12 @@
+﻿namespace CmsRulesQaHarness.API.Models
+{
+    /// <summary>
+    /// Represents the result of a CMS program eligibility determination.
+    /// </summary>
+    public class EligibilityResult
+    {
+        public bool IsEligible { get; set; }
+        public string ProgramCategory { get; set; } = string.Empty;
+        public List<string> Reasons { get; set; } = [];
+    }
+}
