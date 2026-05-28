1619ff2bc65fa76ba00082472425fd3bd707aa55
Jon Sweeney <jonsweeney26@gmail.com>
2026-05-28 10:51:00 -0700

    feat: seed SQLite db

---
+++ CmsRulesQaHarness.API/Data/EligibilityDbContext.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CmsRulesQaHarness.API.Models.Entities;
+
+namespace CmsRulesQaHarness.API.Data
+{
+    public class EligibilityDbContext(DbContextOptions<EligibilityDbContext> options) : DbContext(options)
+    {
+        public DbSet<Household> Households => Set<Household>();
+
+        public DbSet<Applicant> Applicants => Set<Applicant>();
+
+        public DbSet<IncomeRecord> IncomeRecords => Set<IncomeRecord>();
+
+        public DbSet<EligibilityTestCase> EligibilityTestCases => Set<EligibilityTestCase>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Household>(entity =>
+            {
+                entity.HasKey(h => h.HouseholdId);
+
+                entity.Property(h => h.CaseNumber)
+                .IsRequired()
+                .HasMaxLength(25);
+
+                entity.Property(h => h.State)
+                    .IsRequired()
+                    .HasMaxLength(2);
+
+                entity.Property(h => h.County)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.HasMany(h => h.Applicants)
+                    .WithOne(a => a.Household)
+                    .HasForeignKey(a => a.HouseholdId);
+            });
+
+            modelBuilder.Entity<Applicant>(entity =>
+            {
+                entity.HasKey(a => a.ApplicantId);
+
+                entity.Property(a => a.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(a => a.LastName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.HasMany(a => a.IncomeRecords)
+                    .WithOne(i => i.Applicant)
+                    .HasForeignKey(i => i.ApplicantId);
+
+                entity.HasMany(a => a.EligibilityTestCases)
+                    .WithOne(t => t.Applicant)
+                    .HasForeignKey(t => t.ApplicantId);
+            });
+
+            modelBuilder.Entity<IncomeRecord>(entity =>
+            {
+                entity.HasKey(i => i.IncomeRecordId);
+
+                entity.Property(i => i.IncomeType)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(i => i.MonthlyAmount)
+                    .HasPrecision(10, 2);
+            });
+
+            modelBuilder.Entity<EligibilityTestCase>(entity =>
+            {
+                entity.HasKey(t => t.EligibilityTestCaseId);
+
+                entity.Property(t => t.TestCaseNumber)
+                    .IsRequired()
+                    .HasMaxLength(25);
+
+                entity.Property(t => t.ScenarioName)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(t => t.ProgramType)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(t => t.IncomeLimit)
+                    .HasPrecision(10, 2);
+
+                entity.Property(t => t.ExpectedReason)
+                    .IsRequired()
+                    .HasMaxLength(500);
+            });
+        }
+    }
+}
---
+++ CmsRulesQaHarness.Tests/Database/EligibilityDatabaseTests.cs
@@ -0,0 +1,27 @@
+﻿using CmsRulesQaHarness.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsRulesQaHarness.Tests.Database;
+
+public class EligibilityDatabaseTests
+{
+    [Fact]
+    public async Task SeededDatabase_ShouldContainEligibilityTestCases()
+    {
+        var options = new DbContextOptionsBuilder<EligibilityDbContext>()
+            .UseSqlite("Data Source=eligibility-test.db")
+            .Options;
+
+        await using var context = new EligibilityDbContext(options);
+
+        await SeedData.InitializeAsync(context);
+
+        var testCaseCount = await context.EligibilityTestCases.CountAsync();
+        var applicantCount = await context.Applicants.CountAsync();
+        var householdCount = await context.Households.CountAsync();
+
+        Assert.Equal(8, testCaseCount);
+        Assert.Equal(8, applicantCount);
+        Assert.Equal(8, householdCount);
+    }
+}
